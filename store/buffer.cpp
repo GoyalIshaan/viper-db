41c2691a00cb6d017f7cb90d2a3b16f30baa1248
GoyalIshaan <goyalishaan2005@gmail.com>
2025-09-30 15:32:12 -0500

    added a flush buffer

--- store/buffer.cpp
+++ store/buffer.cpp
@@ -1,11 +1,67 @@
 #include "buffer.h"
 #include "store/ndvector.h"
+#include <cstdint>
+#include <cstdio>
 #include <cstring>
+#include <mutex>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <iostream>
+
+
 void NDVectorBuffer::add_vector(const float *new_vector, const char *raw_id) {
+    std::unique_lock lock(mutex);
+
     NDVector vec;
     memcpy(vec.data, new_vector, sizeof(float) * 128);
     memcpy(vec.raw_id, raw_id, sizeof(char) * 37);
 
     buffer[size] = vec;
     size++;
+
+    if (size >= capacity) {
+        is_buffer_full = true;
+        lock.unlock();
+        flush_buffer();
+    }
+}
+
+
+
+void NDVectorBuffer::flush_buffer() {
+    if  (size == 0) return;
+
+    char filename[64];
+    const uint64_t current_file = file_counter.fetch_add(1);
+    snprintf(filename, 64, "%s_%06" PRIu64 ".dat", base_name, current_file);
+
+
+    int fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+    if (fd < 0) {
+        std::cerr << "[ERROR] Failed to open " << filename << "\n";
+        return;
+    };
+
+    size_t bytes_to_write = sizeof(NDVector) * size;
+    ssize_t written = write(fd, buffer, bytes_to_write);
+
+    if (written != (ssize_t) bytes_to_write) {
+        std::cerr << "[ERROR] Partial write! Expected " << bytes_to_write 
+                << " but wrote " << written << "\n";
+        close(fd);
+        return;
+    }
+
+    if (fsync(fd) != 0) {
+        std::cerr << "[ERROR] fsync failed!\n";
+        close(fd);
+        return;
+    }
+
+    close(fd);
+
+
+    size = 0;
+    is_buffer_full = false;
 }
--- store/buffer.h
+++ store/buffer.h
@@ -3,13 +3,18 @@
 
 #include "store/ndvector.h"
 #include <cstddef>
+#include <shared_mutex>
+#include <atomic>
 
 struct NDVectorBuffer {
+
     public :
         bool is_buffer_full = false;
         const size_t capacity = 1000;
         size_t size = 0;
         NDVector *buffer;
+
+        std::shared_mutex mutex;
 
         NDVectorBuffer() {
             buffer = new NDVector[capacity];
@@ -21,7 +26,9 @@ struct NDVectorBuffer {
         void add_vector(const float *new_vector, const char *raw_id);
     
     private :
-        void write_new_file();
+        const char* base_name = "vectors";
+        std::atomic<uint64_t> file_counter{0};
+        void flush_buffer();
 };
 
 #endif
